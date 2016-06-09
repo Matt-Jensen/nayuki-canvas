939973cd6fc699b2471f9156730f31dd5fbdc7e4
Matt-Jensen <matt@jwc.works>
2016-06-08 19:05:48 -0500

    refactor canvas-background

--- src/redraw-canvas/canvas-background.js
+++ src/redraw-canvas/canvas-background.js
@@ -4,21 +4,21 @@
  * @return {Object} (Canvas Background)
  */
 export default function canvasBackground (config) {
-  const data = Object.assign({
+  const defaults = {
     startColor: '#575E85', // TODO make gradient start color configurable
     stopColor: '#2E3145' // TODO make gradient end color configurable
     //, background: '' // TODO allow single color backgrounds
-  }, config);
+  };
 
-  return Object.create({ data }, {
+  return Object.assign(Object.create(null, {
     gradient: {
       get () {
-        const { width, height, size, graphics, startColor, stopColor } = this.data;
+        const { width, height, size, graphics, startColor, stopColor } = this;
         const gradient = graphics.createRadialGradient(width / 2, height / 2, 0, width / 2, height / 2, size / 2);
         gradient.addColorStop(0, startColor);
         gradient.addColorStop(1, stopColor);
         return gradient;
       }
     }
-  });
+  }), defaults, config);
 }
