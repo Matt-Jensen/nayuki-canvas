742db60231945cc238ca46b0d3e735d8e76f715a
Matt-Jensen <matt@jwc.works>
2016-06-10 16:43:08 -0500

    add unit test for

---
+++ tests/unit/initialize.test.js
@@ -0,0 +1,32 @@
+const test = require('tape');
+const initialize = require('../../tmp/initialize');
+
+test('should exist', assert => {
+  const msg = 'exports a module';
+  const actual = !!initialize;
+  const expected = true;
+  assert.equal(actual, expected, msg);
+  assert.end();
+});
+
+test('should invoke instance `next` method', assert => {
+  const msg = 'did invoke `next`';
+
+  let actual = false;
+  const expected = true;
+
+  initialize.call({ next: () => actual = true });
+
+  assert.equal(actual, expected, msg);
+  assert.end();
+});
+
+test('should support chaining', assert => {
+  const msg = 'returned `this`';
+
+  const expected = { next: function () {} };
+  const actual = initialize.call(expected);
+
+  assert.equal(actual, expected, msg);
+  assert.end();
+});
