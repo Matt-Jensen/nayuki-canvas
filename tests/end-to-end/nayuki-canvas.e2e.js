19d35a80e5bcb817fe58fd89b0f4d4e6d555cb93
Matt-Jensen <matt@jwc.works>
2016-06-14 10:21:59 -0500

    add most of the e2e tests

--- tests/end-to-end/nayuki-canvas.e2e.js
+++ tests/end-to-end/nayuki-canvas.e2e.js
@@ -2,30 +2,121 @@ const test = require('tape');
 const nayukiCanvas = require('../../tmp/nayuki-canvas');
 const { createCanvas } = require('../helpers/create');
 const nayukiQuery = require('../helpers/nayuki-query');
+const deepClone = c => JSON.parse(JSON.stringify(c));
 
 test('should render configured background color', assert => {
-  const msg = 'renders configured red HEX background to red RGB';
+  const msg = 'renders red RGB background';
 
-  const canvas = createCanvas(100, 100, { background: '#fff' });
+  const canvas = createCanvas(100, 100, { background: '#ccc' });
   const instance = nayukiCanvas(canvas, { background: '#f44336', numNodes: 0 });
   const nq = nayukiQuery(instance);
 
-  nq.insertCanvas();
-  instance.next(); // invoke one frame render
+  instance.next(); // render frame
   assert.equal(nq.colorAt(1, 1), 'rgba(244,67,54,255)', msg);
 
-  nq.removeCanvas();
   assert.end();
 });
 
-// TODO write remaining E2E tests
-// test('should render correct background radial gradient', assert => {});
-// test('should render correct background linear gradient', assert => {});
-// test('should render correct number of nodes (2)', assert => {});
-// test('should render correct number of edges (3)', assert => {});
-// test('should render correct node color', assert => {});
-// test('should render correct edge color', assert => {});
-// test('should update frame on each `frameInterval`', assert => {});
+test('should render configured background radial gradient', assert => {
+  const msg = 'renders the center red, left black gradient background';
+
+  const canvas = createCanvas(1000, 1000, { background: '#ccc' });
+  const instance = nayukiCanvas(canvas, { background: ['#f44336', '#000'], numNodes: 0 });
+  const nq = nayukiQuery(instance);
+
+  instance.next(); // render frame
+
+  assert.equal(nq.colorAt(500, 500), 'rgba(244,67,54,255)', msg);
+  assert.equal(nq.colorAt(0, 0), 'rgba(0,0,0,255)', msg);
+  assert.end();
+});
+
+test('should render configured background linear gradient', assert => {
+  const msg = 'renders the top red, bottom black linear gradient background';
+
+  const canvas = createCanvas(1000, 1000, { background: '#ccc' });
+  const instance = nayukiCanvas(canvas, { background: ['#f44336', '#000'], gradient: 'linear', numNodes: 0 });
+  const nq = nayukiQuery(instance);
+
+  instance.next(); // render frame
+
+  assert.equal(nq.colorAt(0, 0), 'rgba(244,67,54,255)', msg);
+  assert.equal(nq.colorAt(0, 999), 'rgba(0,0,0,255)', msg);
+  assert.end();
+});
+
+test('should render configured node color', assert => {
+  const msg = 'renders a white node';
+
+  const canvas = createCanvas(100, 100, { background: '#ccc' });
+  const instance = nayukiCanvas(canvas, { background: '#000', numNodes: 1, nodeSize: 100000, nodeColor: '#fff' });
+  const nq = nayukiQuery(instance);
+
+  instance.next(); // generate node
+  Object.assign(instance._nodes[0], { posX: 0.5, posY: 0.5, opacity: 1 }); // place center
+  instance._redrawCanvas(); // draw frame without manipulation
+
+  assert.equal(nq.colorAt(50, 50), 'rgba(255,255,255,255)', msg);
+
+  assert.end();
+});
+
+test('should render configured edge color', assert => {
+  const msg = 'renders white edge';
+
+  const canvas = createCanvas(100, 100, { background: '#ccc' });
+  const instance = nayukiCanvas(canvas, { background: '#000', numNodes: 2, nodeSize: 100, nodeColor: '#000', edgeColor: '#fff', edgeSize: 10000 });
+  const nq = nayukiQuery(instance);
+
+  instance.next();
+  Object.assign(instance._nodes[0], { posX: 0, posY: 0.5, opacity: 1 }); // place center left
+  Object.assign(instance._nodes[1], { posX: 1, posY: 0.5, opacity: 1 }); // place center right
+  Object.assign(instance._edges[0], { nodeA: instance._nodes[0], nodeB: instance._nodes[1], opacity: 1 }); // connect to both nodes
+  instance._redrawCanvas(); // draw frame without manipulation
+
+  assert.equal(nq.colorAt(50, 50), 'rgba(255,255,255,255)', msg);
+  assert.end();
+});
+
+test('should render configured number of nodes', assert => {
+  const msg = 'renders 2 white nodes';
+
+  const canvas = createCanvas(100, 100, { background: '#ccc' });
+  const instance = nayukiCanvas(canvas, { background: '#000', numNodes: 2, nodeSize: 10000, nodeColor: '#fff' });
+  const nq = nayukiQuery(instance);
+
+  instance.next(); // generate nodes
+  Object.assign(instance._nodes[0], { posX: 0, posY: 0.5, opacity: 1 }); // place center left
+  Object.assign(instance._nodes[1], { posX: 1, posY: 0.5, opacity: 1 }); // place center right
+  instance._redrawCanvas(); // draw frame without manipulation
+
+  assert.equal(nq.colorAt(1, 50), 'rgba(255,255,255,255)', msg);
+  assert.equal(nq.colorAt(99, 50), 'rgba(255,255,255,255)', msg);
+
+  assert.end();
+});
+
+test('should update frame on configured `frameInterval`', assert => {
+  const msg = 'canvas frame was updated';
+  assert.plan(1);
+
+  const interval = 10;
+  const canvas = createCanvas();
+  const instance = nayukiCanvas(canvas, { frameInterval: interval });
+
+  instance.next(); // generate initial frame
+  const initial = deepClone(Object.assign({}, { nodes: instance._nodes }, { edges: instance._edges }));
+
+  instance.start();
+
+  setTimeout(function () {
+    const updated = deepClone(Object.assign({}, { nodes: instance._nodes }, { edges: instance._edges }));
+    assert.notDeepEqual(initial, updated, msg);
+    instance.stop();
+    assert.end();
+  }, interval + 1);
+});
+
 // test('should update frame after calling `start`', assert => {});
 // test('should stop updating frame after calling `stop` ', assert => {});
 // test('should remove canvas on `destroy`', assert => {});
--- tests/helpers/nayuki-query.js
+++ tests/helpers/nayuki-query.js
@@ -2,16 +2,6 @@ module.exports = function nayukiQuery (nc) {
   return Object.assign(Object.create({
     colorAt (x, y) {
       return `rgba(${this._context.getImageData(x, y, 1, 1).data.join(',')})`;
-    },
-
-    insertCanvas () {
-      document.body.insertBefore(this._element, document.body.firstChild);
-      return this;
-    },
-
-    removeCanvas () {
-      this._element.parentNode.removeChild(this._element);
-      return this;
     }
   }),
   {
